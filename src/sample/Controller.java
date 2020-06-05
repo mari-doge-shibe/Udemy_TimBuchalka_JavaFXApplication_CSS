557c5aa2cba8a9e3eba7443157b04e30cc0fe131
mari_s <ms.mari.suzuki@gmail.com>
2020-06-05 14:23:33 +0900

    in progress 212. Transforming Nodes and Choosers added drop shadow for button four

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,15 +1,20 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.effect.DropShadow;
 
 public class Controller {
 
     @FXML
     private Label label;
 
-    public void initialize() {
+    @FXML
+    private Button button4;
 
+    public void initialize() {
+        button4.setEffect(new DropShadow());
     }
 
     @FXML
