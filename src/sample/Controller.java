5dd755650d595ebe7165d7a3e04ea5a0aaa1836f
mari_s <ms.mari.suzuki@gmail.com>
2020-06-05 14:15:35 +0900

    in progress 212. Transforming Nodes and Choosers added the initialize method for label

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,17 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class Controller {
+
+    @FXML
+    private Label label;
+
+    public void initialize() {
+        label.setScaleX(2.0);
+        label.setScaleY(2.0);
+    }
+
+
 }
